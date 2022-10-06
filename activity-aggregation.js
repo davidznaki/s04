d69eb77d7835e7177bfce21cb2cae9d220cf024b
davidznaki <calalang1330728@mls.ceu.edu.ph>
2022-10-06 14:43:37 +0800

    Add activity code

---
+++ activity-aggregation.js
@@ -0,0 +1,30 @@
+/* 1 */
+{
+    "_id" : 312456.0,
+    "empName" : "Barry Stevens",
+    "empAge" : 28.0,
+    "jobRole" : "Store Manager",
+    "salary" : 120000.0
+}
+     {
+    "_id" : 5431321.0,
+    "empName" : "Steve Rogers",
+    "empAge" : 26.0,
+    "jobRole" : "Product Supervisor",
+    "salary" : 100000.0
+}
+     {
+    "_id" : 112345.0,
+    "empName" : "Barry Stevens",
+    "empAge" : 32.0,
+    "jobRole" : "Management Manager",
+    "salary" : 140000.0
+}
+     {
+    "_id" : 312456.0,
+    "empName" : "Barry Stevens",
+    "empAge" : 24.0,
+    "jobRole" : "IT Help Support",
+    "salary" : 150000.0
+}
+     
